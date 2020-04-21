dc2cedc7bc4ce7667574bec20718ce9e783e6f6b
Alexis <al.orellano23@gmail.com>
2020-04-20 22:31:47 -0700

    api error enum created

--- DoggieMatch/Networking/APIClient.swift
+++ DoggieMatch/Networking/APIClient.swift
@@ -8,4 +8,20 @@
 
 import UIKit
 
-
+enum APIError: Error {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccessful
+    case jsonParsingFailure
+    
+    var localizedDescription: String {
+        switch self {
+        case .requestFailed: return "Request Failed"
+        case .jsonConversionFailure: return "JSON Conversion Failure"
+        case .invalidData: return "Invalid Data"
+        case .responseUnsuccessful: return "Response Unsuccessful"
+        case .jsonParsingFailure: return "JSON Parsing Failure"
+        }
+    }
+}
